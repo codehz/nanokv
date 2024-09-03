b7b59673429a75f61e7d51043c9233f3bdba6816
codehz <git@hertz.moe>
2024-09-04 03:50:03 +0800

    add simple test

---
+++ npm/nanokv/test.ts
@@ -0,0 +1,61 @@
+import { NanoKV, type KvEntry, type KvQueueEntry } from ".";
+
+const kv = new NanoKV<
+  KvEntry<["test", number], string>,
+  KvQueueEntry<["test-queue"], string>
+>("http://127.0.0.1:2256");
+
+async function watch(signal: AbortSignal) {
+  const watcher = kv.watch(["test", 1], ["test", 2]);
+  const reader = watcher.getReader();
+  signal.addEventListener("abort", () => {
+    reader.cancel();
+  });
+  (async () => {
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break;
+      console.log("update", value);
+    }
+  })();
+}
+
+async function listen(signal: AbortSignal) {
+  const reader = kv.listen(["test-queue"]).getReader();
+  signal.addEventListener("abort", () => {
+    reader.cancel();
+  });
+  (async () => {
+    while (true) {
+      const { done, value } = await reader.readMany();
+      if (done) break;
+      console.log("listen", value);
+      await kv
+        .atomic()
+        .dequeue(...value)
+        .commit();
+    }
+  })();
+}
+
+async function read() {
+  console.log(await kv.getMany(["test", 1], ["test", 2]));
+}
+
+const controller = new AbortController();
+await watch(controller.signal);
+await listen(controller.signal);
+await kv
+  .atomic()
+  .set(["test", 2], "123", { expireIn: 900 })
+  .set(["test", 1], "123", { expireIn: 100 })
+  .enqueue(["test-queue"], "boom", { delay: 700 })
+  .commit();
+await read();
+await Bun.sleep(500);
+await kv.atomic().enqueue(["test-queue"], "boom2").commit();
+await read();
+await Bun.sleep(1000);
+await read();
+await Bun.sleep(1000);
+controller.abort();
