3bbcc6e26cf4dbc86b6c7a7bcdc0a62750d0931c
codehz <git@hertz.moe>
2024-09-04 03:49:55 +0800

    add dist script

---
+++ npm/nanokv/dist.ts
@@ -0,0 +1,30 @@
+import dts from "bun-plugin-dts";
+
+await Bun.$`rm -rf ../dist/*`;
+
+const { success, logs } = await Bun.build({
+  target: "bun",
+  outdir: "../dist",
+  entrypoints: ["./index.ts"],
+  sourcemap: "external",
+  plugins: [dts()],
+  minify: {
+    whitespace: false,
+    syntax: true,
+    identifiers: false,
+  },
+  external: ["flatbuffers"],
+});
+
+if (logs.length) for (const log of logs) console.log(log);
+if (!success) process.exit(1);
+
+const { default: pkg } = await import("./package.json");
+await Bun.write(
+  "../dist/package.json",
+  JSON.stringify({
+    ...pkg,
+    module: "index.js",
+    devDependencies: {},
+  })
+);
