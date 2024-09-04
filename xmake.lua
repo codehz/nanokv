c99b12f1d90098274bb53cfa0ec2e712a2a035fd
codehz <git@hertz.moe>
2024-09-04 19:37:48 +0800

    fix dependencies

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,5 @@
-add_requireconfs("uwebsockets.usockets", {configs= {ssl = "openssl", uring = true}})
-add_requireconfs("uwebsockets", {configs= {zip = true, deflate = true}})
+add_requireconfs("uwebsockets.usockets", {configs = {ssl = "openssl3"}})
+add_requireconfs("uwebsockets", {configs = {zip = true, deflate = true}})
 add_requires("uwebsockets", "leveldb", "spdlog", "flatbuffers", "ctrl-c", "argz")
 add_requires("backward-cpp", {configs = {stack_details = "backtrace_symbol"}, optional = true})
 add_requires("atomic", {system = true, optional = true})
